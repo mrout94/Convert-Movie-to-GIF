304063aa1650193387f7f92f0dad3b8896fd8214
Manabendra Rout <56976500+mrout94@users.noreply.github.com>
2019-10-25 09:18:40 +0530

    Add files via upload

---
+++ Movie_to_GIF.py
@@ -0,0 +1,34 @@
+import imageio
+import os
+import sys
+from subprocess import Popen
+
+SPEED_MULTIPLIER = 5
+CREATE_NO_WINDOW = 0x08000000
+
+def convertFile(inputpath, targetFormat):
+    outputpath = os.path.splitext(inputpath)[0] + targetFormat
+    print("You are converting\r\n\t{0}\r\nto\r\n\t{1}. Thanks for using my program.".format(inputpath, outputpath))
+
+    movie_file = imageio.get_reader(inputpath)
+    fps = movie_file.get_meta_data()['fps']
+
+    gif_file = imageio.get_writer(outputpath, fps=fps)
+    for i,im in enumerate(movie_file):
+        if i % SPEED_MULTIPLIER == 0:
+            sys.stdout.write("\rProcessing Frame number {0}".format(i))
+            sys.stdout.flush()
+            gif_file.append_data(im)
+    gif_file.close()
+    return outputpath
+
+cwd = os.getcwd()
+Video_file = os.path.join(cwd, 'movie_case007.mpg')
+gif_path = convertFile(Video_file, ".gif")
+
+command = 'gifsicle --optimize ' + gif_path + ' --colors 256 --output ' + os.path.join(cwd, 'optimized_gif.gif')
+bat_file = os.path.join(cwd, 'GIF_OPTIMIZER.bat')
+with open(bat_file, 'w+') as f:
+    f.write(command)
+
+print("\nDone.")
